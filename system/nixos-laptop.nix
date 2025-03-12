06fbdedd67dd9e488854eea91ddcb947bd1ea884
Frosty <frosty@archtide.space>
2025-03-12 15:10:42 -0400

    Rename default.nix to version.nix

--- system/nixos-laptop.nix
+++ system/nixos-laptop.nix
@@ -6,12 +6,12 @@
     ./audio.nix
     ./bluetooth.nix
     ./boot.nix
-    ./default.nix
     ./flakes.nix
     ./gc.nix
     ./greetd.nix
     ./networking.nix
     ./users.nix
+    ./version.nix
   ];
 
   services.greetd.settings.default_session.command = lib.mkForce "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --remember-session --cmd Hyprland";
