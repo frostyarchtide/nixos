4cc1256a3079633df9be948550901c05dfae1965
Frosty <frosty@archtide.space>
2025-06-27 22:58:29 -0400

    Update neovim options

--- home/nixvim.nix
+++ home/nixvim.nix
@@ -7,6 +7,21 @@
 
   programs.nixvim = {
     enable = true;
+    opts = {
+      number = true;
+      relativenumber = true;
+      expandtab = true;
+      tabstop = 4;
+      shiftwidth = 4;
+      smartindent = true;
+      ignorecase = true;
+      smartcase = true;
+      cursorline = true;
+      termguicolors = true;
+      scrolloff = 8;
+      sidescrolloff = 8;
+      wrap = false;
+    };
     colorschemes.everforest.enable = true;
     plugins.bufferline.enable = true;
     plugins.lualine.enable = true;
